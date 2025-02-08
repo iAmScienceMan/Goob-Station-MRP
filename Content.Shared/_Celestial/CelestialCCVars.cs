ebee743312584337bb68873ac963b67f8df77a03
insvrg3ncy 3 <92030026+insvrg3ncy@users.noreply.github.com>
2025-02-08 22:24:26 +0300

    Update CelestialCCVars.cs

--- Content.Shared/_Celestial/CelestialCCVars.cs
+++ Content.Shared/_Celestial/CelestialCCVars.cs
@@ -6,13 +6,13 @@ namespace Content.Shared._Celestial.CelestialCCVars;
 public sealed class CelestialCCVars
 {
     /// <summary>
-    /// URL of the Discord webhook which will relay all ahelp messages.
+    /// URL of the Discord webhook which will relay all bans.
     /// </summary>
     public static readonly CVarDef<string> DiscordBanWebhook =
         CVarDef.Create("discord.ban_webhook", string.Empty, CVar.SERVERONLY | CVar.CONFIDENTIAL);
 
     /// <summary>
-    /// URL of the Discord webhook which will relay all ahelp messages.
+    /// URL of the Discord webhook which will relay all rolebans.
     /// </summary>
     public static readonly CVarDef<string> DiscordRoleBanWebhook =
         CVarDef.Create("discord.roleban_webhook", string.Empty, CVar.SERVERONLY | CVar.CONFIDENTIAL);
